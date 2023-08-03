bd06f0dd1d9aa0cd98116c6552cc2262f1846be2
Renata Santos <123095086+Re-Santos@users.noreply.github.com>
2023-08-02 22:02:34 -0300

    Initial commit

---
+++ src/Router/Router.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mercadinho from "../Pages/Mercadinho";
+import Carrinho from "../Pages/Carrinho";
+export default function Router() {
+  const [carrinho, setCarrinho] = useState([]);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Mercadinho carrinho={carrinho} setCarrinho={setCarrinho} />}
+        />
+        <Route
+          path="/cart"
+          element={<Carrinho carrinho={carrinho} setCarrinho={setCarrinho} />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
